3099e7af54bb24d380050be13bca50667f9bf3c6
Eugene Verbovoi <63497261+verbovoi@users.noreply.github.com>
2020-09-23 21:16:16 +0300

    Добавляет решение 8 задачи

---
+++ js/task-08.js
@@ -0,0 +1,29 @@
+const inputEl = document.querySelector('#controls input');
+const createBtnEL = document.querySelector('button[data-action="render"]');
+const destroyBtnEL = document.querySelector('button[data-action="destroy"]')
+const boxEl = document.querySelector('#boxes');
+
+
+const createBoxes = function (amount) {
+    amount = Number(inputEl.value);
+    console.log(amount);
+    let boxSizes = 30;
+
+    for (let i = 1; i <= amount; i++) {
+        const newBoxEl = document.createElement('div');
+        newBoxEl.style.width = `${boxSizes}px`;
+        newBoxEl.style.height = `${boxSizes}px`;
+        newBoxEl.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+        boxEl.appendChild(newBoxEl);
+        boxSizes += 10;
+    }
+}
+
+const destroyBoxes = function () {
+    console.log('click');
+    boxEl.innerHTML = '';
+
+}
+
+createBtnEL.addEventListener('click', createBoxes);
+destroyBtnEL.addEventListener('click', destroyBoxes);
